3bc55e8fe8221d6f675a93184874b64c60b6b9e5
Nabeel El-Husayni <nabeel@ccs.neu.edu>
2017-01-25 15:47:57 -0500

    LVM-286: Adds role management.
    Note: This does not enforce any permission rules, just adds the
    ability to do so since those rules will be fleshed out later.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,32 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  # Role management as follows
+  # 0 => tutor (default)
+  # 1 => coordinator
+  # 2 => admin
+  def tutor?
+    role.zero?
+  end
+
+  def coordinator?
+    role == 1
+  end
+
+  def admin?
+    role == 2
+  end
+
+  def tutor_level?
+    role >= 0
+  end
+
+  def coordinator_level?
+    role >= 1
+  end
+
+  def admin_level?
+    role >= 2
+  end
 end
