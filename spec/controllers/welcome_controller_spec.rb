5e5b286c64261c723f3a0d7155ddfc2ac826c00d
Zack Youngren <zack@youngren.io>
2017-01-30 15:20:42 -0500

    LVM-266: Implement tests for Welcome controller
    https://cs5500-jira.ccs.neu.edu/browse/LVM-266
    
    * Add a sweet gem for testing controllers!
    * Update some config stuff
      * Remove stock comments

---
+++ spec/controllers/welcome_controller_spec.rb
@@ -0,0 +1,37 @@
+require 'rails_helper'
+
+RSpec.describe WelcomeController, type: :controller do
+  describe '#index' do
+    describe 'when authenticated' do
+      before do
+        sign_in_auth
+      end
+
+      it 'renders the index template' do
+        get :index
+        expect(response).to render_template(:index)
+      end
+
+      it 'returns success status' do
+        get :index
+        expect(response.status).to eq(200)
+      end
+    end
+
+    describe 'when not authenticated' do
+      before do
+        sign_in_auth nil
+      end
+
+      it 'redirects to the sign in page' do
+        get :index
+        expect(response).to redirect_to(new_user_session_path)
+      end
+
+      it 'returns redirect status' do
+        get :index
+        expect(response.status).to eq(302)
+      end
+    end
+  end
+end
