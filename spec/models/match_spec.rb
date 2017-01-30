80534a75288e571697b75d06b8b595e29783694b
Zack Youngren <zack@youngren.io>
2017-01-30 08:54:32 -0500

    LVM-277: Implement tests for Match model
    https://cs5500-jira.ccs.neu.edu/browse/LVM-277

--- spec/models/match_spec.rb
+++ spec/models/match_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Match, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'associations' do
+    it 'should belong to a tutor' do
+      should belong_to(:tutor)
+    end
+
+    it 'should belong to a student' do
+      should belong_to(:student)
+    end
+  end
 end
