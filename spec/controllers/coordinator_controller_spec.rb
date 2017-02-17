51b6cbb193fddac8964b0ae41f5608a3dd39217d
Joseph Lally <lally.j@husky.neu.edu>
2017-02-17 10:39:21 -0500

    coordinator factory implementation

--- spec/controllers/coordinator_controller_spec.rb
+++ spec/controllers/coordinator_controller_spec.rb
@@ -6,18 +6,44 @@ RSpec.describe CoordinatorsController, type: :controller do
       sign_in_auth
     end
 
-    describe 'GET #show' do
+    describe 'GET #index' do
       before do
-        @coordinator = create(:coordinator)
+        @coordinator = create(:full_coordinator)
       end
 
       it 'populates the specified coordinator' do
-        get :show, id: @coordinator
+        get :show, params: { id: @coordinator }
         expect(assigns(:coordinator)).to eq(@coordinator)
       end
 
+      it 'renders the :index view' do
+        get :index
+        expect(response).to render_template :index
+      end
+    end
+
+    describe 'GET #show' do
+      before do
+        @coordinator = create(:full_coordinator)
+      end
+
+      it 'populates the specified coordinator' do
+        get :show, params: { id: @coordinator }
+        expect(assigns(:coordinator)).to eq(@coordinator)
+      end
+
+      it "populates the specified coordinator's students" do
+        get :show, params: { id: @coordinator }
+        expect(assigns(:students)).to eq(@coordinator.students)
+      end
+
+      it "populates the specified coordinator's tutors" do
+        get :show, params: { id: @coordinator }
+        expect(assigns(:tutors)).to eq(@coordinator.tutors)
+      end
+
       it 'renders the :show view' do
-        get :show, id: @coordinator
+        get :show, params: { id: @coordinator }
         expect(response).to render_template :show
       end
     end
@@ -40,12 +66,12 @@ RSpec.describe CoordinatorsController, type: :controller do
       end
 
       it 'populates the specified coordinator' do
-        get :edit, id: @coordinator
+        get :edit, params: { id: @coordinator }
         expect(assigns(:coordinator)).to eq(@coordinator)
       end
 
       it 'renders the :edit view' do
-        get :edit, id: @coordinator
+        get :edit, params: { id: @coordinator }
         expect(response).to render_template :edit
       end
     end
@@ -57,17 +83,17 @@ RSpec.describe CoordinatorsController, type: :controller do
 
       context 'with valid attributes' do
         it 'saves the new coordinator in the database' do
-          post :create, coordinator: @coordinator_attrs
+          post :create, params: { coordinator: @coordinator_attrs }
           expect(assigns(:coordinator)).to be_persisted
         end
 
         it 'assigns the newly created coordinator as @coordinator' do
-          post :create, coordinator: @coordinator_attrs
+          post :create, params: { coordinator: @coordinator_attrs }
           expect(assigns(:coordinator)).to be_a(Coordinator)
         end
 
         it 'redirects to the newly created coordinator view' do
-          post :create, coordinator: @coordinator_attrs
+          post :create, params: { coordinator: @coordinator_attrs }
           expect(response).to redirect_to(Coordinator.last)
         end
       end
@@ -78,12 +104,12 @@ RSpec.describe CoordinatorsController, type: :controller do
         end
 
         it 'assigns a newly created but unsaved purchase as @purchase' do
-          post :create, coordinator: @coordinator_attrs
+          post :create, params: { coordinator: @coordinator_attrs }
           expect(assigns(:coordinator)).to be_a_new(Coordinator)
         end
 
         it 're-renders the :new view' do
-          post :create, coordinator: @coordinator_attrs
+          post :create, params: { coordinator: @coordinator_attrs }
           expect(response).to render_template :new
         end
       end
@@ -97,18 +123,18 @@ RSpec.describe CoordinatorsController, type: :controller do
 
       context 'with valid attributes' do
         it 'saves the new coordinator in the database' do
-          post :update, id: @coordinator.id, coordinator: @new_coordinator_attrs
+          post :update, params: { id: @coordinator.id, coordinator: @new_coordinator_attrs }
           expect(Coordinator.last.name).to eq(@new_coordinator_attrs[:name])
         end
 
         it 'assigns the updated coordinator as @coordinator' do
-          post :update, id: @coordinator.id, coordinator: @new_coordinator_attrs
+          post :update, params: { id: @coordinator.id, coordinator: @new_coordinator_attrs }
           expect(assigns(:coordinator).name)
             .to eq(@new_coordinator_attrs[:name])
         end
 
         it 'redirects to the coordinator view' do
-          post :update, id: @coordinator.id, coordinator: @new_coordinator_attrs
+          post :update, params: { id: @coordinator.id, coordinator: @new_coordinator_attrs }
           expect(response).to redirect_to(@coordinator)
         end
       end
@@ -119,12 +145,16 @@ RSpec.describe CoordinatorsController, type: :controller do
         end
 
         it 'assigns the existing coordinator as @coordinator' do
-          post :update, id: @coordinator, coordinator: @new_coordinator_attrs
+          post :update, params: {
+            id: @coordinator, coordinator: @new_coordinator_attrs
+          }
           expect(assigns(:coordinator)).to eq(@coordinator)
         end
 
         it 're-renders the :edit view' do
-          post :update, id: @coordinator, coordinator: @new_coordinator_attrs
+          post :update, params: {
+            id: @coordinator, coordinator: @new_coordinator_attrs
+          }
           expect(response).to render_template :edit
         end
       end
@@ -133,12 +163,12 @@ RSpec.describe CoordinatorsController, type: :controller do
     describe 'DELETE #destroy' do
       it 'destroys the coordinator' do
         coordinator = create(:coordinator)
-        expect { delete :destroy, id: coordinator }
+        expect { delete :destroy, params: { id: coordinator } }
           .to change(Coordinator, :count).by(-1)
       end
       it 'redirects to the :index view' do
         coordinator = create(:coordinator)
-        delete :destroy, id: coordinator
+        delete :destroy, params: { id: coordinator }
         expect(response).to redirect_to coordinators_path
       end
     end
--- spec/factories/coordinator.rb
+++ spec/factories/coordinator.rb
@@ -3,4 +3,29 @@ FactoryGirl.define do
     name { Faker::Name.name }
     email { Faker::Internet.email }
   end
+
+  factory :coordinator_with_student, class: Coordinator do
+    name { Faker::Name.name }
+    email { Faker::Internet.email }
+    after(:create) do |coordinator|
+      coordinator.students << FactoryGirl.create(:student)
+    end
+  end
+
+  factory :coordinator_with_tutor, class: Coordinator do
+    name { Faker::Name.name }
+    email { Faker::Internet.email }
+    after(:create) do |coordinator|
+      coordinator.tutors << FactoryGirl.create(:tutor)
+    end
+  end
+
+  factory :full_coordinator, class: Coordinator do
+    name { Faker::Name.name }
+    email { Faker::Internet.email }
+    after(:create) do |coordinator|
+      coordinator.tutors << FactoryGirl.create(:tutor)
+      coordinator.students << FactoryGirl.create(:student)
+    end
+  end
 end
