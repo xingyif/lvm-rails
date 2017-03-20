439e4cf5a6e5f77b6e80403451130c3829aa8067
Yifan Xing <xing.yif@husky.neu.edu>
2017-03-20 00:38:12 -0400

    Created Tutoring_Session Migration and Implemented Model

--- app/models/match.rb
+++ app/models/match.rb
@@ -2,5 +2,4 @@ class Match < ApplicationRecord
   belongs_to :tutor
   belongs_to :student
 
-  has_many :affiliates
 end
---
+++ app/models/tutoring_session.rb
@@ -0,0 +1,9 @@
+class TutoringSession < ApplicationRecord
+  belongs_to :tutor
+  belongs_to :student
+
+  validates :location,     presense: true
+  validates :start_time,   presense: true
+  validates :end_time,     presense: true
+
+end
