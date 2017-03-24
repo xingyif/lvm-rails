38294f78b5adb3381c6a9f5974f318cd76ff5d1c
Yifan Xing <xing.yif@husky.neu.edu>
2017-03-23 23:24:38 -0400

    Implemednted TutoringSession Controller

---
+++ app/controllers/tutoring_sessions_controller.rb
@@ -0,0 +1,51 @@
+class TutoringSessionsController < ApplicationController
+  before_action :set_tutoring_session, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @tutoring_session = TutoringSession.all
+  end
+
+  def show; end
+
+  def new
+    @tutoring_session = TutoringSession.new
+  end
+
+  def edit; end
+
+  def create
+    @tutoring_session = TutoringSession.new(tutoring_session_params)
+
+    if @tutoring_session.save
+      redirect_to @tutoring_session,
+                  notice: 'Tutoring session was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @tutoring_session.update(tutoring_session_params)
+      redirect_to @tutoring_session,
+                  notice: 'Tutoring session was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @tutoring_session.destroy
+    redirect_to tutoring_session_url,
+                notice: 'Tutoring session was successfully destroyed.'
+  end
+
+  private
+
+  def set_tutoring_session
+    @tutoring_session = TutoringSession.find(params[:id])
+  end
+
+  def tutoring_session_params
+    params.require(:tutoring_session).permit(:location, :start_time, :end_time)
+  end
+end
