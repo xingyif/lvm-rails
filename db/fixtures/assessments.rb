d6dad913cad73c8571cb3a005d267c34fe6ed1c2
Joseph Lally <lally.j@husky.neu.edu>
2017-03-27 19:43:38 -0400

    LVM 444 fix Assessments fixture file for reseeding purposes (#95)
    * removed reference to tutor_id
    
    * instead of commenting out line, delete line
    
    * removed reference to tutor at "shuffle" of tutors

--- db/fixtures/assessments.rb
+++ db/fixtures/assessments.rb
@@ -1,7 +1,6 @@
 require 'assessments_helper'
 
 students = [*1..20].shuffle
-tutors = [*1..20].shuffle
 
 20.times do
   Assessment.seed do |a|
@@ -9,6 +8,5 @@ tutors = [*1..20].shuffle
     a.date = Faker::Date.between(10.years.ago, Date.today)
     a.score = Random.rand(100.0).truncate(2)
     a.student_id = students.pop
-    a.tutor_id = tutors.pop
   end
 end
