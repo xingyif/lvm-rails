ca1f421507174bafd2e3b5499911946e33061419
Joseph Lally <lally.j@husky.neu.edu>
2017-03-31 11:46:16 -0400

    LVM-458 Update Coordinator Show view (#105)
    * tests for updated coordinator show view
    
    * update coordinator show view
    
    * moved email address and phone number in to upper panel
    
    * removed repeated name field

--- spec/views/coordinators/show_spec.rb
+++ spec/views/coordinators/show_spec.rb
@@ -1,11 +1,25 @@
 require 'rails_helper'
+require 'spec_helper'
 
-RSpec.describe 'coordinators/show' do
+RSpec.describe 'coordinators/show', type: :view do
   before(:each) do
-    @coordinator = assign(:coordinator, create(:coordinator))
+    @coordinator = create(:coordinator, phone_number: '(999) 999-9999')
   end
 
-  it 'displays the attributes in <p>' do
+  it 'renders attributes in <p>' do
     render
+    expect(rendered).to match(@coordinator.name)
+    expect(rendered).to match(/\(999\) 999-9999/)
+    expect(rendered).to match(@coordinator.email)
+  end
+
+  it 'contains the Back link' do
+    render
+    assert_select 'a[href=?]', coordinators_path
+  end
+
+  it 'contains the Edit link' do
+    render
+    assert_select 'a[href=?]', edit_coordinator_path(@coordinator)
   end
 end
